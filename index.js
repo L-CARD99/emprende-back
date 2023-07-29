f7513581c0e7be7723238d70a1a3deb8472ac08b
L-CARD99 <leoortega536@gmail.com>
2023-07-29 14:22:48 -0600

    Agregamos package.json

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const http = require("http"); //Funcion require, para importar codigo/librerias de terceros, o tambien de nosotros.
 
-console.log(http);
+//console.log(http);
 
 //const nameExportsAnother = require("./another");
 //console.log({ nameExportsAnother });
 
 function requestController() {
   //Logica de nuestra funcion
-  console.log("Hola Mundo");
+  console.log("Hola Mundo!!!");
 }
 
 //Configurar nuestro servidor
 const server = http.createServer(requestController);
 
-server.listen(4200);
+server.listen(4300);
